92d3de8b876ffe7c086bcbaa949b2823bb8f5efe
Russell Elliott <russell@quadsquad.net>
2023-10-08 16:20:42 -0700

    more style; mathcing that of the speakers

--- src/views/Home/PreviousHackathons/index.view.tsx
+++ src/views/Home/PreviousHackathons/index.view.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import { PreviousHackathonData } from '../../../Props/PreviousHackathonProps/props'; // Adjust the import path as needed
+import React, { useState } from "react"
+/*import SpeakerCard, {
+  SpeakerData,
+} from "../../../components/SpeakerCard/SpeakerCard"*/
 import { nanoid } from 'nanoid';
+
+import "./index.scss"
+import PreviousHackathon from "../../../components/PreviousHackathons"
+
+import { PreviousHackathonData } from '../../../Props/PreviousHackathonProps/props'; // Adjust the import path as needed
 
 interface PreviousHackathonsProps {
   hackathons: PreviousHackathonData[];
 }
 
-const PreviousHackathons: React.FC<PreviousHackathonsProps> = ({ hackathons }) => (
-  <div className='previous-hackathons'>
-    <h2>Previous Hackathons</h2>
-    <ul className='previous-hackathons--list'>
-      {hackathons.map((hackathon, index) => (
-        <li key={nanoid()} className='previous-hackathons--item'>
-          <h3>{hackathon.name}</h3>
-          <p>
-            Link: <a href={hackathon.link} target='_blank' rel='noopener noreferrer'>{hackathon.link}</a>
-          </p>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const PreviousHackathons: React.FC<PreviousHackathonsProps> = ({ hackathons }) => {
+  const [page, setPage] = useState<number>(0);
 
-export default PreviousHackathons;
+  return (
+    <div className='speakers'>
+      <div className='speakers__container'>
+        <div className='speakers__container__header'>PREVIOUS HACKATHONS</div>
+        <div className='speakers__container__cards'>
+          {hackathons.map(data => (
+            <PreviousHackathon
+              key={nanoid()}
+              data={data}
+              style={{
+                transform: `translateX(calc(-${page * 100}% - ${page * 40}px))`,
+              }}
+            />
+          ))}
+          <button
+            className='speakers__container__left-button'
+            onClick={() => {
+              page > 0 ? setPage(page - 1) : setPage(0);
+            }}
+          ></button>
+          <button
+            className='speakers__container__right-button'
+            onClick={() => {
+              page < hackathons.length - 1 ? setPage(page + 1) : setPage(hackathons.length - 1);
+            }}
+          ></button>
+        </div>
+        <div className=''></div>
+      </div>
+    </div>
+  );
+};
+
+
+export default PreviousHackathons
