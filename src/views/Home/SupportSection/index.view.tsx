da45512fc031b8ac9dbda2275a815a74bac27801
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2023-10-14 00:26:36 -0700

    updated links

--- src/views/Home/SupportSection/index.view.tsx
+++ src/views/Home/SupportSection/index.view.tsx
@@ -6,10 +6,7 @@ import { ReactComponent as Twitter } from "../../../assets/icons/Twitter.svg"
 import { ReactComponent as Discord } from "../../../assets/icons/Discord.svg"
 import "./index.scss"
 import { SocialButton } from "../../../components/Button/SocialButton"
-import {
-    SocialButtonInputs,
-    SocialButtonProps,
-  } from "../../../Props/Socials/props"
+
 
 
 const SupportSection: React.FC = () => (
@@ -23,10 +20,10 @@ const SupportSection: React.FC = () => (
             <div className='SupportSection__container--SupportContainer--icons'>
 
                 <SocialButton logo={Instagram} link={"https://www.instagram.com/cruzhacks/"} key={"https://www.instagram.com/cruzhacks/"} />
-                <SocialButton logo={Facebook} link={"https://www.instagram.com/cruzhacks/"} key={"https://www.instagram.com/cruzhacks/"} />
-                <SocialButton logo={LinkedIn} link={"https://www.instagram.com/cruzhacks/"} key={"https://www.instagram.com/cruzhacks/"} />
-                <SocialButton logo={Twitter} link={"https://www.instagram.com/cruzhacks/"} key={"https://www.instagram.com/cruzhacks/"} />
-                <SocialButton logo={Discord} link={"https://www.instagram.com/cruzhacks/"} key={"https://www.instagram.com/cruzhacks/"} />
+                <SocialButton logo={Facebook} link={"https://www.facebook.com/CruzHacks/"} key={"https://www.facebook.com/CruzHacks/"} />
+                <SocialButton logo={LinkedIn} link={"https://www.linkedin.com/company/cruzhacks/"} key={"https://www.linkedin.com/company/cruzhacks/"} />
+                <SocialButton logo={Twitter} link={"https://twitter.com/CruzHacks"} key={"https://twitter.com/CruzHacks"} />
+                <SocialButton logo={Discord} link={"https://discord.gg/NBUHWbmaeb"} key={"https://discord.gg/NBUHWbmaeb"} />
 
             </div>
         </div>
